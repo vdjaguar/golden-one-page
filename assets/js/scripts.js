d78b075e68eb401e11112805661882875cfd7b50
vdjaguar <vdjaguar@gmail.com>
2020-11-24 20:54:24 +0200

    inited_fancybox_plugin

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+	/*Fancybox*/
+		$('[data-fancybox="gallery"]').fancybox({
+			thumbs : {
+				autoStart : false
+			}
+		});
+		
+		// Animation
+		$.fancybox.defaults.animationEffect = "zoom-in-out";
+	/*Fancybox/ */
+});
